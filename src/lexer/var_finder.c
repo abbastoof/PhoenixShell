6bed2a049da5e148786f7d581fa624a0ba467b36
Mojtaba Toof <mtoof@c2r6p6.hive.fi>
2023-06-01 16:10:41 +0300

    norminette var_finder

--- src/lexer/var_finder.c
+++ src/lexer/var_finder.c
@@ -3,29 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   var_finder.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 11:16:33 by mtoof             #+#    #+#             */
-/*   Updated: 2023/06/01 15:46:35 by atoof            ###   ########.fr       */
+/*   Updated: 2023/06/01 16:09:33 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	checker(char *str, t_lexer *state, int var_flag)
+{
+	if (var_flag == 1 && !ft_isspace(str[state->i + 1]) && str[state->i
+			+ 1] != '\0' && str[state->i + 1] != '\"'
+		&& str[state->i + 1] != '\\')
+		state->i++;
+	else if (var_flag == 1 && (!ft_isspace(str[state->i + 1]) || str[state->i
+				+ 1] == '\0' || str[state->i + 1] == '\"'))
+	{
+		state->i++;
+		return (2);
+	}
+	return (0);
+}
 
 char	*var_finder(char *str, t_lexer *state, t_environment *env, int var_flag)
 {
 	int		indx;
 	char	*des;
 
-	if (var_flag == 1 && !ft_isspace(str[state->i + 1]) && str[state->i
-			+ 1] != '\0' && str[state->i + 1] != '\"' && str[state->i + 1] != '\\')
-		state->i++;
-	else if (var_flag == 1 && (!ft_isspace(str[state->i + 1]) || str[state->i
-				+ 1] == '\0' || str[state->i + 1] == '\"'))
-	{
-		state->i++;
+	if (checker(str, state, var_flag) == 2)
 		return ("$");
-	}
 	indx = state->i;
 	if (ft_isdigit(str[indx]) && var_flag == 1)
 	{
@@ -35,13 +43,13 @@ char	*var_finder(char *str, t_lexer *state, t_environment *env, int var_flag)
 	des = ft_calloc(1, 1);
 	if (!des)
 		return (NULL);
-	while (!ft_isspace(str[indx]) && str[indx] != '\0' && !ft_isquote(str[indx]))
+	while (!ft_isspace(str[indx]) && str[indx] != '\0'
+		&& !ft_isquote(str[indx]))
 		indx++;
 	state->var = ft_strnjoin(des, (str + state->i), indx - state->i);
 	free(des);
 	des = NULL;
 	state->i = indx;
-	printf("str = %s\n", str + state->i);
 	if (var_flag == 1)
 		return (find_path(env->env_var, state->var));
 	else
