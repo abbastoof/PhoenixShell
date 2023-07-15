99bac08e688b2f1498a04d5db015c2b0b4f88bed
mtoof <mojtaba.toof@gmail.com>
2023-07-15 03:50:14 -0700

    rename files inside src/tree folder

--- src/built-in/env.c
+++ src/built-in/env.c
@@ -6,7 +6,7 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:22:43 by atoof             #+#    #+#             */
-/*   Updated: 2023/07/12 14:01:40 by mtoof            ###   ########.fr       */
+/*   Updated: 2023/07/15 03:49:03 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_env(t_env *env)
 		if (ft_strchr(env->env_var[indx], '='))
 		{
 			ft_putstr(env->env_var[indx]);
-			write(1, "\n", 2);
+			write(1, "\n", 1);
 		}
 		indx++;
 	}
