2fb1b4d89da2e073862e757a8f69c6f23659d5da
Mojtaba Toof <mtoof@c3r2p5.hive.fi>
2023-07-17 14:16:37 +0300

    unset.c

--- src/built-in/unset.c
+++ src/built-in/unset.c
@@ -6,7 +6,7 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 12:22:43 by atoof             #+#    #+#             */
-/*   Updated: 2023/07/14 15:06:58 by mtoof            ###   ########.fr       */
+/*   Updated: 2023/07/17 14:10:30 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static void	handle_error(char *str)
 	ft_putstr("': not a valid identifier\n");
 }
 
-static char	**shrink_env(t_env *env, char *str)
+static void	shrink_env(t_env *env, char *str)
 {
 	char	**new_var;
 	int		index;
@@ -29,31 +29,40 @@ static char	**shrink_env(t_env *env, char *str)
 	if (!new_var)
 	{
 		ft_putstr("Malloc error\n");
-		return (NULL);
+		return ;
 	}
 	while (env->env_var[index] != NULL)
 	{
-		if (str != NULL && ft_strncmp(env->env_var[index], str, ft_strlen(str)) == 0)
+		if (ft_strncmp(env->env_var[index], str, ft_strlen(str)) == 0)
 			index++;
-		new_var[index] = ft_strdup(env->env_var[index]);
-		index++;
+		if (env->env_var[index] != NULL)
+		{
+			new_var[index] = ft_strdup(env->env_var[index]);
+			index++;
+		}
 	}
-	free_double_ptr(env->env_var);
-	return (new_var);
+	if (env->env_var != NULL)
+		free_double_ptr(env->env_var);
+	env->counter--;
+	env->env_var = new_var;
 }
 
-static char	**find_var_inside_env(char *str, t_env *env, int index)
+static void	find_var_inside_env(char *str, t_env *env)
 {
-	char	**tmp;
+	int		index;
 
-	tmp = NULL;
-	if (ft_strncmp(env->env_var[index], str, ft_strlen(str)) == 0)
+	index = 0;
+	if (!str || !env->env_var)
+		return ;
+	while (env->env_var[index] != NULL)
 	{
-		tmp = shrink_env(env, str);
-		if (tmp == NULL)
-			return (NULL);
+		if (ft_strncmp(str, env->env_var[index], ft_strlen(str)) == 0)
+		{
+			shrink_env(env, str);
+			break ;
+		}
+		index++;
 	}
-	return (tmp);
 }
 
 void	ft_unset(char **args, t_env *env)
@@ -66,9 +75,7 @@ void	ft_unset(char **args, t_env *env)
 		if (ft_strchr(args[index], '='))
 			handle_error(args[index]);
 		else
-		{
-			env->env_var = find_var_inside_env(args[index], env, index);
-		}
+			find_var_inside_env(args[index], env);
 		index++;
 	}
 }
