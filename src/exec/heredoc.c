f12fe95f59950f335cec32bb1328f3f0ae035d09
Abbas Toof <atoof@c3r2p3.hive.fi>
2023-07-17 14:17:59 +0300

    exec_heredoc_complete

--- src/exec/heredoc.c
+++ src/exec/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 15:19:45 by atoof             #+#    #+#             */
-/*   Updated: 2023/07/17 14:05:39 by atoof            ###   ########.fr       */
+/*   Updated: 2023/07/17 14:16:23 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,17 @@ void	exec_heredoc(t_tree *tree)
 	line = readline("> ");
 	while (line != NULL)
 	{
-		if (ft_strncmp(line, tree->redir->file_name, \
-		ft_strlen(tree->redir->file_name)) == 0)
+		if (ft_strncmp(line, tree->redir->file_name,
+				ft_strlen(tree->redir->file_name)) == 0)
 		{
 			free(line);
 			break ;
 		}
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		add_history(line);
+		free(line);
+		line = readline("> ");
 	}
+	close(fd);
 }
