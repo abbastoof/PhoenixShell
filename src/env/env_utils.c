16b07ca0402b11c21ad77bc93b17b65af2c9963d
Mojtaba Toof <mtoof@c3r3p3.hive.fi>
2023-07-21 18:47:59 +0300

    env_char_ptr and some error handling

--- src/env/env_utils.c
+++ src/env/env_utils.c
@@ -6,7 +6,7 @@
 /*   By: mtoof <mtoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 11:58:25 by mtoof             #+#    #+#             */
-/*   Updated: 2023/07/20 12:53:04 by mtoof            ###   ########.fr       */
+/*   Updated: 2023/07/21 18:45:37 by mtoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,46 @@ char	*shelvl_value(char *value)
 		return (ft_itoa(res));
 	}
 	return (NULL);
+}
+
+static void	*error_handling(void)
+{
+	ft_putstr_fd("Malloc env_char_ptr\n", 2);
+	return (NULL);
+}
+
+char	**env_char_ptr(t_env **env)
+{
+	char	**ptr;
+	int		index;
+	t_env	*tmp;
+
+	if (!env)
+		return (NULL);
+	ptr = ft_calloc(sizeof(char *), ft_listsize(env));
+	if (!ptr)
+		return (NULL);
+	tmp = *env;
+	index = 0;
+	while (tmp != NULL)
+	{
+		if (tmp->key != NULL && tmp->value == NULL)
+		{
+			ptr[index] = ft_strdup(tmp->key);
+			if (!ptr[index])
+				return (error_handling());
+		}
+		else if (tmp->key != NULL && tmp->value != NULL)
+		{
+			ptr[index] = ft_strjoin(tmp->key, ft_strjoin("=", tmp->value));
+			if (!ptr[index])
+				return (error_handling());
+		}
+		index++;
+		tmp = tmp->next;
+	}
+
+	return (ptr);
 }
 
 int	add_back_env(t_env **lst, t_env *new_node)
