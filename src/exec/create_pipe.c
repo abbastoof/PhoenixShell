07db6b76c1e903796c356a6018d0f4d9636b8f09
Abbas Toof <atoof@c1r5p11.hive.fi>
2023-08-04 14:55:51 +0300

    fixing pipe

--- src/exec/create_pipe.c
+++ src/exec/create_pipe.c
@@ -6,7 +6,7 @@
 /*   By: atoof <atoof@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:26:35 by atoof             #+#    #+#             */
-/*   Updated: 2023/08/04 12:53:49 by atoof            ###   ########.fr       */
+/*   Updated: 2023/08/04 14:53:14 by atoof            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,28 +41,38 @@
 
 void	create_pipe(t_tree **tree, t_env **env)
 {
-	int	end[2];
+	int		pipe_fds[2];
+	pid_t	pid_left;
+	pid_t	pid_right;
 
-	if (pipe(end) < 0)
+	if (pipe(pipe_fds) < 0)
 		exit(1);
-	if (child_process() == 0)
+	pid_left = fork();
+	if (pid_left < 0)
+		exit(1);
+	if (pid_left == 0)
 	{
-		if (dup2(end[FD_WRITE_END], STDOUT_FILENO) < 0)
+		close(pipe_fds[FD_READ_END]);
+		if (dup2(pipe_fds[FD_WRITE_END], STDOUT_FILENO) < 0)
 			exit(1);
-		close(end[FD_READ_END]);
+		close(pipe_fds[FD_WRITE_END]);
 		exec_tree(&(*tree)->left, env);
 		exit(g_exit_status);
 	}
-	wait(&(g_exit_status));
-	if (child_process() == 0)
+	pid_right = fork();
+	if (pid_right < 0)
+		exit(1);
+	if (pid_right == 0)
 	{
-		if (dup2(end[FD_READ_END], STDIN_FILENO) < 0)
+		close(pipe_fds[FD_WRITE_END]);
+		if (dup2(pipe_fds[FD_READ_END], STDIN_FILENO) < 0)
 			exit(1);
-		close(end[FD_WRITE_END]);
+		close(pipe_fds[FD_READ_END]);
 		exec_tree(&(*tree)->right, env);
 		exit(g_exit_status);
 	}
-	close(end[FD_READ_END]);
-	close(end[FD_WRITE_END]);
-	wait(&g_exit_status);
+	close(pipe_fds[FD_READ_END]);
+	close(pipe_fds[FD_WRITE_END]);
+	waitpid(pid_left, &g_exit_status, 0);
+	waitpid(pid_right, &g_exit_status, 0);
 }
